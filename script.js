cad3cf1066559b6fb5081bfb3badf3e9b5ed091d
Vishu Verma <visheshbaund@gmail.com>
2024-05-13 14:32:02 +0530

    adding media queries for the responsicity of game

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let time = 60;
+let time = 0;
 let score = 0;
 let count = 0;
 let missed = 0;
@@ -6,45 +6,43 @@ let number;
 
 // Call this function when the DOM is loaded
 window.onload = function () {
-    main();
-}
+  main();
+};
 
-// function to make bubbles 
+// function to make bubbles
 
-function makeBubble(){
-    let clutter = "";
+function makeBubble() {
+  let clutter = "";
 
-for(let i=0; i<98; i++){
-    let number = Math.floor(Math.random()*10);
+  for (let i = 0; i < 98; i++) {
+    let number = Math.floor(Math.random() * 10);
     clutter += `<div id="circle">${number}</div>`;
-}
+  }
 
-document.querySelector("#bottombox").innerHTML = clutter;
+  document.querySelector("#bottombox").innerHTML = clutter;
 }
 
 // function for timer
 
-function timer(){
-   let interval =  setInterval( () => {
-        if(time > 0){
-            time--;
-            document.querySelector("#timer").textContent = time;
-            if(time < 10){
-                document.querySelector("#timer").style.color = "red"
-            }
-        }
-        else{
-            clearInterval(interval);
-            document.querySelector("#timer").style.color = "#fff"
-            endGame();
-        }
-    },1000)
+function timer() {
+  let interval = setInterval(() => {
+    if (time > 0) {
+      time--;
+      document.querySelector("#timer").textContent = time;
+      if (time < 10) {
+        document.querySelector("#timer").style.color = "red";
+      }
+    } else {
+      clearInterval(interval);
+      document.querySelector("#timer").style.color = "#fff";
+      endGame();
+    }
+  }, 1000);
 }
-
 
 // End game function
 function endGame() {
-    document.querySelector("#bottombox").innerHTML = `
+  document.querySelector("#bottombox").innerHTML = `
         <div id="result">
             <h1>Game Over</h1>
             <h2>Total Score: ${score}</h2>
@@ -52,74 +50,67 @@ function endGame() {
             <h2>Missed: ${missed}</h2>
             <button id='newGame'>New Game</button>
         </div>`;
-    document.querySelector('#rmNum').textContent = "0";
-    document.querySelector("#score").textContent = '0';
+  document.querySelector("#rmNum").textContent = "0";
+  document.querySelector("#score").textContent = "0";
 
-    // Add event listener to "New Game" button
-    document.querySelector("#newGame").addEventListener("click", () => {
-        restartGame();
-    });
+  // Add event listener to "New Game" button
+  document.querySelector("#newGame").addEventListener("click", () => {
+    restartGame();
+  });
 }
 
 // function for generate random number
 
-function randomNumber(){
-    number = Math.floor(Math.random()*10);
-    document.querySelector('#rmNum').textContent = number;
+function randomNumber() {
+  number = Math.floor(Math.random() * 10);
+  document.querySelector("#rmNum").textContent = number;
 }
 
-// function to increase score 
+// function to increase score
 
-function incScore(){
-    score += 10;
-    document.querySelector('#score').textContent = score;
+function incScore() {
+  score += 10;
+  document.querySelector("#score").textContent = score;
 }
 
 // funtion for bubbles click
 
 function hitBubble() {
-    document.querySelector("#bottombox").addEventListener("click", (dets) => {
-        //    console.log(dets.target.textContent);
-        if(number === Number(dets.target.textContent)){
-                incScore();
-                count++;
-                randomNumber();
-                makeBubble();
-        }
-        else{
-            missed++;
-            if(dets.target.textContent < 10){
-                dets.target.style.backgroundColor = "red";
-                setTimeout(function() {
-                    makeBubble();
-                    randomNumber();
-                  }, 150)
-            }
-        }
-        })
+  document.querySelector("#bottombox").addEventListener("click", (dets) => {
+    //    console.log(dets.target.textContent);
+    if (number === Number(dets.target.textContent)) {
+      incScore();
+      count++;
+      randomNumber();
+      makeBubble();
+    } else {
+      missed++;
+      if (dets.target.textContent < 10) {
+        dets.target.style.backgroundColor = "red";
+        setTimeout(function () {
+          makeBubble();
+          randomNumber();
+        }, 150);
+      }
+    }
+  });
 }
 
-// funciton for start new game 
+// funciton for start new game
 
 function restartGame() {
-    time = 60;
-    score = 0;
-    count = 0;
-    missed = 0;
-    main();
+  time = 60;
+  score = 0;
+  count = 0;
+  missed = 0;
+  main();
 }
 
-// father of all functions 
+// father of all functions
 
-function main(){
-    makeBubble();
-    timer();
-    randomNumber();
-    hitBubble();
+function main() {
+  makeBubble();
+  timer();
+  randomNumber();
+  hitBubble();
 }
-
-
-
-
-
-
